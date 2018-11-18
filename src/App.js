124805b502b1a06f79a7fbc1183b9fcde426ae57
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-18 09:52:13 -0600

    added middleware and store

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => {}, {}, applyMiddleware);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is a React client that gets and posts to JSONPlaceholder API.
-          </p>
-          <p> Type something into the form.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <PostForm />
-        <hr/>
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              This is a React client that gets and posts to JSONPlaceholder API.
+            </p>
+            <p> Type something into the form.</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <PostForm />
+          <hr/>
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
